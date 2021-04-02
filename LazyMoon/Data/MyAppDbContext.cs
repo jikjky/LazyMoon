a51010c0ebd83e3bd946e2b5ae1fe067d3c9d561
jikjky gmail.com <jikjky@gmail.com>
2021-04-02 13:48:02 +0900

    초기 커밋.

---
+++ LazyMoon/Data/MyAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LazyMoon.Data
+{
+    public class MyAppDbContext : IdentityDbContext
+    {
+        public MyAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
