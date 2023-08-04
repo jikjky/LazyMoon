e1515f7dc092c3b99a056afd7774cb892536aa25
lazymoon <lazymoon@twosun.com>
2023-08-04 11:50:55 +0900

    Paint 페이지 추가

---
+++ LazyMoon/JavaScript.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+
+namespace LazyMoon
+{
+    public static class JavaScript
+    {
+        public delegate void GetValueChange(double lineWidth, string strokeStyle);
+
+        public static GetValueChange? GetValueChangeEvent;
+
+        public static double LineWidth { get; set; }
+        public static string StrokeStyle { get; set; } = string.Empty;
+
+        [JSInvokable]
+        public static void ValueChanged(double lineWidth, string strokeStyle)
+        {
+            LineWidth = lineWidth;
+            StrokeStyle = strokeStyle;
+            if (GetValueChangeEvent != null)
+            {
+                GetValueChangeEvent.Invoke(lineWidth, strokeStyle);
+            }
+        }
+    }
+}
