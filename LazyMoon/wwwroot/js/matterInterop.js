86fe85235bab1b25ebabef0b3a69b1e202f76923
lazymoon <lazymoon@twosun.com>
2023-08-28 17:23:07 +0900

    javascript로 이미지 처리하게 변경

--- LazyMoon/wwwroot/js/matterInterop.js
+++ LazyMoon/wwwroot/js/matterInterop.js
@@ -42,7 +42,7 @@ window.initializeSimulation = (containerId) => {
         }
     });
 
-    
+
     engine.gravity.x = 0;
     engine.gravity.y = 0.5;
 
@@ -96,4 +96,94 @@ window.addBox = (src, w, h) => {
 
         }, i * 100);
     }
+}
+
+window.initializeCanvas = () => {
+
+
+    const canvas = document.createElement('canvas');
+    canvas.width = 1000;
+    canvas.height = 100;    
+
+    const ctx = canvas.getContext('2d');
+
+    window.toBase64Image = async (text) => {
+        ctx.clearRect(0,0,1000,100);
+        const fontPath = 'font/NotoSansKR-Regular-Hestia.otf'; // Update this path
+        const randomColor = `rgb(${getRandomNumber(256)}, ${getRandomNumber(256)}, ${getRandomNumber(256)})`;
+
+        ctx.fillStyle = 'rgba(5, 5, 5, 0)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.font = "25px NotoSans";
+        ctx.strokeStyle = 'rgb(0, 0, 0)';
+        ctx.fillStyle = randomColor;
+        ctx.textAlign = 'left';
+        var x = 0;
+        var y = 30;
+        var lineheight = 30;
+        var lines = text.split('\n');
+        for (var i = 0; i < lines.length; i++)
+            ctx.fillText(lines[i], x, y + (i * lineheight));
+
+        const rect = findNonBackgroundRectangle(ctx, { r: 5, g: 5, b: 5, a: 0 }); // Update background color
+        const cropImage = cropCanvas(ctx, rect.x, rect.y, rect.width, rect.height);
+
+        const result = await convertCanvasToBase64(cropImage);
+        return JSON.stringify({ base64: result, width: rect.width, height: rect.height });
+    }
+
+    function findNonBackgroundRectangle(ctx, backgroundColor) {
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+        let minX = canvas.width;
+        let minY = canvas.height;
+        let maxX = -1;
+        let maxY = -1;
+
+        for (let y = 0; y < canvas.height; y++) {
+            for (let x = 0; x < canvas.width; x++) {
+                const index = (y * canvas.width + x) * 4;
+                const pixel = {
+                    r: data[index],
+                    g: data[index + 1],
+                    b: data[index + 2],
+                    a: data[index + 3],
+                };
+                if (pixel.a != 0) {
+                    minX = Math.min(minX, x);
+                    minY = Math.min(minY, y);
+                    maxX = Math.max(maxX, x);
+                    maxY = Math.max(maxY, y);
+                }
+            }
+        }
+
+        const width = maxX - minX + 1;
+        const height = maxY - minY + 1;
+        return { x: minX, y: minY, width, height };
+    }
+
+    function cropCanvas(ctx, x, y, width, height) {
+        const croppedCanvas = document.createElement('canvas');
+        croppedCanvas.width = width;
+        croppedCanvas.height = height;
+        const croppedCtx = croppedCanvas.getContext('2d');
+        const imageData = ctx.getImageData(x, y, width, height);
+        croppedCtx.putImageData(imageData, 0, 0);
+        return croppedCanvas;
+    }
+
+    async function convertCanvasToBase64(canvas) {
+        return canvas.toDataURL('image/png');
+    }
+
+    function getRandomNumber(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function compareColors(color1, color2) {
+        return color1.r === color2.r && color1.g === color2.g && color1.b === color2.b && color1.a === color2.a;
+    }
+
 }
