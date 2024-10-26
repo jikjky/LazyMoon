6e3da158af601328eb035a7fd98932ed22e3119c
lazymoon <jikjky@gmail.com>
2024-10-27 01:55:54 +0900

    중복집계 수정

--- LazyMoon/Service/ServerCouter.cs
+++ LazyMoon/Service/ServerCouter.cs
@@ -1,30 +1,34 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 namespace LazyMoon.Service
 {
     public sealed class ServerCounterService
     {
-        public int CounterValue { get; private set; }
+        List<string> guids = new List<string>();
+        public int CounterValue { get { return guids.Count; } }
         private DateTime LastResetDate;
 
         public ServerCounterService()
         {
-            CounterValue = 0;
             LastResetDate = GetKoreaTime().Date;
         }
 
-        public void Add()
+        public void Add(string guid)
         {
+            if (!guids.Contains(guid))
+            {
+                guids.Add(guid);
+            }
             DateTime koreaTime = GetKoreaTime();
 
             // 날짜가 바뀌면 카운터 초기화
             if (koreaTime.Date != LastResetDate)
             {
-                CounterValue = 0;
+                guids.Clear();
                 LastResetDate = koreaTime.Date;
             }
 
-            CounterValue++;
         }
 
         private DateTime GetKoreaTime()
